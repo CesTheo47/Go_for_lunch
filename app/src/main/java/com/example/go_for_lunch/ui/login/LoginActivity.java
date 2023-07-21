10be0827f9044ebcf75ea003663a77fcf56c21fe
theophile cestaro <theophilecestaro96@gmail.com>
2023-07-21 17:48:15 +0200

    create firebase layout + activity

---
+++ app/src/main/java/com/example/go_for_lunch/ui/login/LoginActivity.java
@@ -0,0 +1,82 @@
+package com.example.go_for_lunch.ui.login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.go_for_lunch.R;
+import com.example.go_for_lunch.ui.main.MainActivity;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.android.gms.common.Scopes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            (result) -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if (user != null) {
+                        Toast.makeText(LoginActivity.this, "Bienvenue, " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Connexion réussie, mais impossible d'obtenir les informations de l'utilisateur.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Connexion impossible, veuillez réessayer.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    private void startSignIn() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                /*new AuthUI.IdpConfig.EmailBuilder().build(),       */
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+
+        AuthMethodPickerLayout customLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.activity_firebase)
+                .setGoogleButtonId(R.id.googleSignInButton)
+                .setEmailButtonId(R.id.facebookLoginButton)
+                .build();
+
+        AuthUI.IdpConfig googleIdp = new AuthUI.IdpConfig.GoogleBuilder()
+                .setScopes(Arrays.asList(Scopes.GAMES))
+                .build();
+
+        AuthUI.IdpConfig facebookIdp = new AuthUI.IdpConfig.FacebookBuilder()
+                .setPermissions(Arrays.asList("user_friends"))
+                .build();
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(googleIdp,facebookIdp))
+                .build();
+
+        signInLauncher.launch(signInIntent);
+    }
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Toast.makeText(LoginActivity.this, "Bienvenue, " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(LoginActivity.this, "Connexion réussie, mais impossible d'obtenir les informations de l'utilisateur.", Toast.LENGTH_SHORT).show();
+            startSignIn();
+        }
+    }
+    git add
+}
--- app/src/main/java/com/example/go_for_lunch/ui/main/MainActivity.java
+++ app/src/main/java/com/example/go_for_lunch/ui/main/MainActivity.java
@@ -30,34 +30,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
-            new FirebaseAuthUIActivityResultContract(),
-            (result) -> {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    if (user != null) {
-                        Toast.makeText(MainActivity.this, "Bienvenue, " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Connexion réussie, mais impossible d'obtenir les informations de l'utilisateur.", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Connexion impossible, veuillez réessayer.", Toast.LENGTH_SHORT).show();
-                }
-            });
 
-    private void startSignIn() {
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                /*new AuthUI.IdpConfig.EmailBuilder().build(),       */
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().build()
-        );
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setTheme(R.style.GreenTheme)
-                .build();
-
-        signInLauncher.launch(signInIntent);
-    }
 
     private ActivityMainBinding binding;
 
@@ -72,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(binding.bottomNavView, navController);
     }
-
-    
 
 
 }
