6496c235e9708199241d8a209dacaeab36e36679
theophile cestaro <theophilecestaro96@gmail.com>
2023-08-03 16:12:35 +0200

    correction bottom nav

--- app/src/main/java/com/example/go_for_lunch/ui/main/MainActivity.java
+++ app/src/main/java/com/example/go_for_lunch/ui/main/MainActivity.java
@@ -5,6 +5,7 @@ import static androidx.activity.result.ActivityResultCallerKt.registerForActivit
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -12,22 +13,30 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.go_for_lunch.R;
 import com.example.go_for_lunch.databinding.ActivityMainBinding;
+import com.example.go_for_lunch.ui.main.fragment.MapsFragment;
+import com.example.go_for_lunch.ui.main.fragment.RestaurantListFragment;
+import com.example.go_for_lunch.ui.main.fragment.WorkmatesListFragment;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this,navController, binding.drawerLayout);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        /*NavigationUI.setupWithNavController(binding.bottomNavView, navController);*/
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
     }
 
     private void initToolBar() {
