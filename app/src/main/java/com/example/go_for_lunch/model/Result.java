1a4ef618af7a5fc25629902bb8d63cf5595d3f93
theophile cestaro <theophilecestaro96@gmail.com>
2023-09-05 00:49:08 +0200

    add api model

---
+++ app/src/main/java/com/example/go_for_lunch/model/Result.java
@@ -0,0 +1,210 @@
+
+package com.example.go_for_lunch.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Result {
+
+    @SerializedName("business_status")
+    @Expose
+    private String businessStatus;
+    @SerializedName("geometry")
+    @Expose
+    private Geometry geometry;
+    @SerializedName("icon")
+    @Expose
+    private String icon;
+    @SerializedName("icon_background_color")
+    @Expose
+    private String iconBackgroundColor;
+    @SerializedName("icon_mask_base_uri")
+    @Expose
+    private String iconMaskBaseUri;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("opening_hours")
+    @Expose
+    private OpeningHours openingHours;
+    @SerializedName("photos")
+    @Expose
+    private List<Photo> photos;
+    @SerializedName("place_id")
+    @Expose
+    private String placeId;
+    @SerializedName("plus_code")
+    @Expose
+    private PlusCode plusCode;
+    @SerializedName("price_level")
+    @Expose
+    private int priceLevel;
+    @SerializedName("rating")
+    @Expose
+    private double rating;
+    @SerializedName("reference")
+    @Expose
+    private String reference;
+    @SerializedName("scope")
+    @Expose
+    private String scope;
+    @SerializedName("types")
+    @Expose
+    private List<String> types;
+    @SerializedName("user_ratings_total")
+    @Expose
+    private int userRatingsTotal;
+    @SerializedName("vicinity")
+    @Expose
+    private String vicinity;
+    @SerializedName("permanently_closed")
+    @Expose
+    private boolean permanentlyClosed;
+
+    public String getBusinessStatus() {
+        return businessStatus;
+    }
+
+    public void setBusinessStatus(String businessStatus) {
+        this.businessStatus = businessStatus;
+    }
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIconBackgroundColor() {
+        return iconBackgroundColor;
+    }
+
+    public void setIconBackgroundColor(String iconBackgroundColor) {
+        this.iconBackgroundColor = iconBackgroundColor;
+    }
+
+    public String getIconMaskBaseUri() {
+        return iconMaskBaseUri;
+    }
+
+    public void setIconMaskBaseUri(String iconMaskBaseUri) {
+        this.iconMaskBaseUri = iconMaskBaseUri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public OpeningHours getOpeningHours() {
+        return openingHours;
+    }
+
+    public void setOpeningHours(OpeningHours openingHours) {
+        this.openingHours = openingHours;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public PlusCode getPlusCode() {
+        return plusCode;
+    }
+
+    public void setPlusCode(PlusCode plusCode) {
+        this.plusCode = plusCode;
+    }
+
+    public int getPriceLevel() {
+        return priceLevel;
+    }
+
+    public void setPriceLevel(int priceLevel) {
+        this.priceLevel = priceLevel;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+
+    public int getUserRatingsTotal() {
+        return userRatingsTotal;
+    }
+
+    public void setUserRatingsTotal(int userRatingsTotal) {
+        this.userRatingsTotal = userRatingsTotal;
+    }
+
+    public String getVicinity() {
+        return vicinity;
+    }
+
+    public void setVicinity(String vicinity) {
+        this.vicinity = vicinity;
+    }
+
+    public boolean isPermanentlyClosed() {
+        return permanentlyClosed;
+    }
+
+    public void setPermanentlyClosed(boolean permanentlyClosed) {
+        this.permanentlyClosed = permanentlyClosed;
+    }
+
+}
