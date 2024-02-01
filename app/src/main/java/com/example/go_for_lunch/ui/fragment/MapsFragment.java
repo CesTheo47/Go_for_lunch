d5b1a4d19dafb245d52a462b1a5b45fab1431add
theophile cestaro <theophilecestaro96@gmail.com>
2024-02-01 18:38:43 +0100

    maps fragment logic

--- app/src/main/java/com/example/go_for_lunch/ui/fragment/MapsFragment.java
+++ app/src/main/java/com/example/go_for_lunch/ui/fragment/MapsFragment.java
@@ -1,22 +1,50 @@
 package com.example.go_for_lunch.ui.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+
 import com.example.go_for_lunch.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_maps, container, false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Obtenir la référence du FragmentContainerView
+        FragmentContainerView fragmentContainerView = view.findViewById(R.id.map);
+
+        // Initialiser la carte Google Maps
+        initMap(fragmentContainerView);
+    }
+
+    private void initMap(FragmentContainerView fragmentContainerView) {
+        if (fragmentContainerView != null) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+            if (mapFragment != null) {
+                mapFragment.getMapAsync(this);
+            }
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // add Marqueur here
     }
 }
